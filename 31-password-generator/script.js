1928ffec279ac6ace2e4a0fd48415eeaee9ff798
Rodrigo Martins <rmlopes2006@hotmail.com>
2022-08-23 20:43:47 -0300

    Random Functions

---
+++ 31-password-generator/script.js
@@ -0,0 +1,28 @@
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// console.log(getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// console.log(getRandomNumber());
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()=<>/,.';
+
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+// console.log(getRandomSymbol());
