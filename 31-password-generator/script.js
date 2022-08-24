e0d105698f1a0c7c8e6b7560600e74d146e972d0
Rodrigo Martins <rmlopes2006@hotmail.com>
2022-08-24 19:36:11 -0300

    Generate Password

--- 31-password-generator/script.js
+++ 31-password-generator/script.js
@@ -1,8 +1,51 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
+}
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  // console.log(length);
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  // console.log(hasUpper, hasLower, hasNumber, hasSymbol);
+  resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  // console.log(typesCount);
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+  // console.log(typesArr);
+  if (typesCount === 0) {
+    return '';
+  }
+  for (let i = 0; i < length; i+=typesCount) {
+    typesArr.forEach(type => {
+      // console.log(type);
+      const funcName = Object.keys(type)[0];
+      // console.log(funcName);
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
 }
 
 function getRandomLower() {
