20b9de03e66df6de840ce629e33509b61283eeb6
Rodrigo Martins <rmlopes2006@hotmail.com>
2022-08-31 21:17:16 -0300

    Save Notes To Local Storage

--- 33-notes-app/script.js
+++ 33-notes-app/script.js
@@ -1,5 +1,12 @@
 const addBtn = document.getElementById('add');
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+console.log(notes);
+
+if (notes) {
+  notes.forEach(note => addNewNote(note));
+}
+
 addBtn.addEventListener('click', () => addNewNote());
 
 function addNewNote(text = '') {
@@ -30,6 +37,8 @@ function addNewNote(text = '') {
 
   deleteBtn.addEventListener('click', () => {
     note.remove();
+
+    updateLS();
   });
 
   editBtn.addEventListener('click', () => {
@@ -41,7 +50,22 @@ function addNewNote(text = '') {
     const { value } = e.target;
 
     main.innerHTML = marked(value);
+    updateLS();
   });
 
   document.body.appendChild(note);
+}
+
+// localStorage.setItem('name', 'Brad');
+// localStorage.getItem('name');
+// localStorage.removeItem('name');
+
+function updateLS() {
+  const notesText = document.querySelectorAll('textarea');
+
+  const notes = [];
+  notesText.forEach(note => notes.push(note.value));
+
+  // console.log(notes);
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
