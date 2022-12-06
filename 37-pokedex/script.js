ba5a0cc267608ce4095dbaad49f461c4c035f83d
Rodrigo Martins <rmlopes2006@hotmail.com>
2022-12-06 07:49:23 -0300

    Creating The Pokemon Cards

--- 37-pokedex/script.js
+++ 37-pokedex/script.js
@@ -17,6 +17,9 @@ const colors = {
 	normal: '#F5F5F5'
 };
 
+const main_types = Object.keys(colors);
+// console.log(main_types);
+
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
     await getPokemon(i);
@@ -27,7 +30,38 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  
+  // console.log(data);
+  createPokemonCard(data);
+}
+
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, '0');
+
+  const poke_types = pokemon.types.map(type => type.type.name);
+  // console.log(poke_types);
+  const type = main_types.find(type => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+    <div class="image-container">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="">
+    </div>
+    <div class="info">
+      <span class="number">#${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span></small>
+    </div>
+  `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
 }
 
 fetchPokemons();
